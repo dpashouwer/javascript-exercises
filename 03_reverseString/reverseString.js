53c6e009a4ccb38d6be18707b48dfe473b074c22
dpashouwer <dpashouwer@gmail.com>
2022-07-19 18:28:45 -0600

    Completed first two exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    strContainer = [];
+    for (let i = 0; i < str.length; i++) {
+        strContainer.push(str[i])
+    }
+    return strContainer.reverse().join("")
 };
 
 // Do not edit below this line
