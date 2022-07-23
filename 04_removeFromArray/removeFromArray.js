b5db55ed53486b96364c16e52e6b6979d22b8e40
dpashouwer <dpashouwer@gmail.com>
2022-07-23 10:01:43 -0600

    Completed removeFromArray.js.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j <= array.length - 1; j++) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
