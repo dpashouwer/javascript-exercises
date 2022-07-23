e7f4e5ffd8d27a28fba1d94f84b5f88dfa258210
dpashouwer <dpashouwer@gmail.com>
2022-07-23 10:56:45 -0600

    Completed tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+  convertedValue = (value - 32) * 5/9; 
+  convertedValue = Number(convertedValue.toFixed(1));
+  return convertedValue
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+  convertedValue = (value * 9/5) + 32; 
+  convertedValue = Number(convertedValue.toFixed(1));
+  return convertedValue
 };
 
 // Do not edit below this line
