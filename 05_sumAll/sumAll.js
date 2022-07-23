a30b544f720a3f96f3110027d16809a282d011c1
dpashouwer <dpashouwer@gmail.com>
2022-07-23 10:13:57 -0600

    Completed sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    const lowNum = Math.min(startNum, endNum); 
+    const highNum = Math.max(startNum, endNum); 
+
+    let total = 0; 
+
+    if (startNum < 0 | endNum < 0) {
+        return 'ERROR';
+    } else if (typeof startNum != 'number' | typeof endNum != 'number') {
+        return 'ERROR';
+    } else {
+        for (let i = lowNum; i <= highNum; i++) {
+            total = total + i;
+        }
+    }
+
+
+    return total; 
 };
 
 // Do not edit below this line
